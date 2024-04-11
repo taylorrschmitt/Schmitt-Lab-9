26d1d8216894cca3e752334243e67ac9fea2bab7
prism-bjj <154019624+prism-bjj@users.noreply.github.com>
2024-04-11 13:34:47 -0400

    Added the decoder function and changed a bit of the encoder so that it returned the string instead of printing it immedietly

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 def encoder(code):
+    estr = ''
     for i in range(0, len(code)):
-        digit = int(i)
+        digit = int(code[i])
         digit += 3
         if digit > 9:
             digit %= 10
-        print(digit, end=" ")
+        estr += str(digit)
+    return estr
+
+def decoder(encoded_pass):
+    dstr = ''
+    for i in range(0, len(encoded_pass)):
+        digit = int(encoded_pass[i])
+        digit -= 3
+        if digit < 0:
+            digit += 10
+            dstr += str(digit)
+        else:
+            dstr += str(digit)
+    return dstr
+
 
 def menu():
     print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
@@ -12,9 +27,19 @@ def menu():
 if __name__ == '__main__':
     menu()
     choice = input("Please enter an option: ")
-    if choice == "1":
-        password = input("Please enter your password to ancode: ")
-        encoded_pass = encoder(password)
-        print("Your password has been encoded and stored!")
-    if choice == "2":
-        print(f"The encoded password is {encoded_pass}, and the original password is {password}.")
+    while True:
+        if choice == "1":
+            password = input("Please enter your password to encode: ")
+            encoded = encoder(password)
+            print(encoder(password))
+            print("\nYour password has been encoded and stored!\n")
+            menu()
+            choice = input("Please enter an option: ")
+
+        if choice == "2":
+            print(f"The encoded password is {encoded}, and the original password is {decoder(encoded)}.")
+            menu()
+            choice = input("Please enter an option: ")
+
+        if choice == "3":
+            break
